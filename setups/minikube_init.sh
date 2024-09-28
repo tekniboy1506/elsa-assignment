27f2fb59e0fd72bdec21c31bc8f0085abda35053
Thanh Phan <thanh.phan@aspireapp.com>
2024-09-28 18:54:58 +0700

    Refine minikube init

--- setups/minikube_init.sh
+++ setups/minikube_init.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-
-docker network create --driver=bridge --subnet=192.168.49.0/24 minikube 
-minikube start --network minikube 
-minikube addons enable ingress
-
 echo "Installing Local Docker Registry..."
 docker run -d -p 5000:5000 --restart=always --name registry registry:2
-echo REGISTRY_URL=192.168.49.1:5000 >> .env
+
+
+docker network create --driver=bridge --subnet=192.168.49.0/24 minikube 
+minikube start --network minikube --insecure-registry="192.168.49.1:5000"
+minikube addons enable ingress
 
 echo "Installing MongoDB into Minikube..."
 kubectl config set-context --current --namespace default
 kubectl apply -f setups/mongodb
+
+echo REGISTRY_URL=192.168.49.1:5000 >> .env
