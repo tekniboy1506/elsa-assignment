6b2d6914665a0cfd78d62e68c6b5949fa8034872
Thanh Phan <thanh.phan@aspireapp.com>
2024-09-28 16:49:52 +0700

    Added script for Docker build

--- setups/minikube_init.sh
+++ setups/minikube_init.sh
@@ -9,4 +9,6 @@ kubectl apply -f setups/docker-registry/docker-registry.yml
 kubectl wait --for=condition=Ready pod/local-registry
 echo REGISTRY_URL=$(minikube ip):=$(kubectl get svc local-registry-service -o jsonpath='{.spec.ports[0].nodePort}') >> .env
 
+echo "Installing MongoDB into Minikube..."
 kubectl config set-context --current --namespace default
+kubectl apply -f setups/mongodb
